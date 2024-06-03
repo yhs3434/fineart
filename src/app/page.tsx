4ab7b3ccdc6c8d0d093371c6f0271ebc9f533dfa
Hansol Yoon <yoondev3434@gmail.com>
2024-06-04 00:27:52 +0900

    feat: Add app bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
 
 export default function Home() {
     return (
         <main className={styles.main}>
-            <span>app index</span>
+            <AppBar>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        Typography
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
         </main>
     );
 }
