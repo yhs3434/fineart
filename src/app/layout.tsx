7d9d3b4e97cd5cc0fd29a9806cd6508028f0af65
Hansol Yoon <yoondev3434@gmail.com>
2024-06-04 00:35:28 +0900

    refactor: AppBar is moved into layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +22,30 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ko">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            Typography
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+                {children}
+            </body>
         </html>
     );
 }
