14372bfed7554334ce96644eba56083886e31a11
Hansol Yoon <yoondev3434@gmail.com>
2024-06-04 00:49:27 +0900

    feat: index page html create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,35 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Typography, Container, Button } from "@mui/material";
 
 export default function Home() {
-    return <main className={styles.main}></main>;
+    return (
+        <main className={styles.main}>
+            <Typography>
+                Which car brand you want to make price list?
+            </Typography>
+            <Container>
+                <Button
+                    startIcon={
+                        <Image
+                            src="/assets/images/hyundai_logo.png"
+                            alt="hyundai"
+                            width={240}
+                            height={153}
+                        />
+                    }
+                />
+                <Button
+                    startIcon={
+                        <Image
+                            src="/assets/images/kia_logo.png"
+                            alt="kia"
+                            width={240}
+                            height={153}
+                        />
+                    }
+                />
+            </Container>
+        </main>
+    );
 }
